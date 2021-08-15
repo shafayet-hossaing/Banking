781796833360e9cb5b1accd2605748411084da9e
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-08-15 12:39:58 +0600

    added functionality at main bank

--- JS/mainbank.js
+++ JS/mainbank.js
@@ -2,6 +2,7 @@
 const depositButton = document.getElementById("depositButton")
 depositButton.addEventListener("click", () => {
 
+    // Working with deposit board
     // Selecting deposit input
     const newDepositAmount = document.getElementById("newDepositAmount")
     let newDepositAmountValue = newDepositAmount.value
@@ -16,8 +17,47 @@ depositButton.addEventListener("click", () => {
     // Assigning the new value of total deposit
     currentDepositText.innerText = newUpdatedDepositText
 
+
+
+    // Working with balance board
     // Selecting balance text
+    const balance = document.getElementById("balance")
+    let currentBalance = balance.innerText
+
+    // adding the updated deposit amount with balance
+    let addingNewCurrentBalance = parseFloat(currentBalance) + parseFloat(newDepositAmountValue)
+
+    // Updated balance amount
+    balance.innerText = addingNewCurrentBalance
 
     // Clearing Deposit Input Field
     newDepositAmount.value = ""
+})
+
+
+// Withdraw handling
+const withdrawButton = document.getElementById("withdrawButton")
+withdrawButton.addEventListener("click", () => {
+    
+
+    // Selecting Withdraw Input
+    const newWithdrawAmount = document.getElementById("newWithdraw")
+    const newWithdrawAmountText = newWithdrawAmount.value
+
+    // Selecting withdraw text
+    const currentWithdraw = document.getElementById("withdraw")
+    const currentWithdrawText = currentWithdraw.innerText
+
+    // Adding new withdraw with current withdraw
+    const newUpdatedWithdrawAmount = parseFloat(currentWithdrawText) + parseFloat(newWithdrawAmountText)
+    currentWithdraw.innerText = newUpdatedWithdrawAmount
+
+    // Selecting balance text
+    const balance = document.getElementById("balance")
+    let currentBalance = balance.innerText
+
+    // Subtracting new withdraw amount from balance
+    const newUpdatedBalanceAmount = currentBalance - newWithdrawAmountText
+    balance.innerText = newUpdatedBalanceAmount
+
 })
